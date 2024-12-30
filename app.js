881f4b12bd659180490bfc575cf7e4bd47bafd34
Ayşe Nur Duvan <nurayseduvan1@icloud.com>
2024-12-30 13:19:33 +0300

    commit 2

--- app.js
+++ app.js
@@ -3,10 +3,30 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-            ilkDegisken: 'Helloşş'
+            name: 'Ayse Nur ',
+            a: 3,
+            b: 4,
+            job: 'Engineer',
+            website: "https://gokhunyayla.com",
+            websiteTag: '<a href="https://webtangular.com">BU da  Html tag eklenerek benim sitem</a>',
+            age: 23,
+            giyim: {
+                sapka: true,
+                pantolon: 'Kot',
+                tshirt: false,
+            },
+            yetenek: ["Sürücü", "Yazılımcı", "Boksör", "Yüzücü"]
         }
     },
     methods: {
+        hesapla: function (isim, gun_durumu) {
+            return "İyi " + gun_durumu + " " + this.name;
+        },
+
+        topla: function () {
+            return this.a + this.b;
+
+        }
 
     }
 }).mount("#First");
